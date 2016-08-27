403c45c46dfaae6b16d9e6bcfd4545c0e6ebeac9
cuong <cuongpv88@live.com>
2016-08-27 21:03:37 +0700

    x

---
+++ cuongpham.org/src/Lib.Core/UrlHelpers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lib.Core
+{
+    public static class UrlHelpers
+    {
+        public static string UrlSchema()
+        {
+
+            return null;
+        }
+    }
+}
